ee420d19a5affbe76fa6d6ef3edb9f152fb94ea8
AndrewElvisDeng <adeng1433@gmail.com>
2018-07-25 13:34:52 -0500

    Revert "fixed merge issues, back to password recovery"
    This reverts commit e5b0f2ab85e51b5d48660f8171edf396e8a89e21.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2018_07_24_200002) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
