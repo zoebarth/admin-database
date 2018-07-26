ffd5bab7e89f276c3d7581ff693fa3b6ab907a19
Zoe Barth <zoe.barth@gmail.com>
2018-07-26 11:30:54 -0500

    fixed back button

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2018_07_24_200002) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
