44ea3a2436c5232c5cbe0a13913e20406ed13d5c
Zoe Barth <zoe.barth@gmail.com>
2018-07-26 11:59:13 -0500

    list of admin users added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_scope :admin do 
     get '/admins/sign_out' => 'devise/sessions#destroy'
   end
+
+  resources :admins, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'bodies#index'
 
