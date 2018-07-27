9a63953c3a0cc42196480aef2ca5e6331775f32c
Zoe Barth <zoe.barth@gmail.com>
2018-07-27 15:22:59 -0500

    seed deleted

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
- zoe = Admin.create({email: 'zoe.barth@gmail.com'})
