e86171e267f7e161b8901faea373bb4cd55700f0
AndrewElvisDeng <adeng1433@gmail.com>
2018-07-27 09:35:02 -0500

    actions limited to index only

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    resources :bodies, :fishes
+    resources :bodies, :fishes, :only => [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'bodies#index'
