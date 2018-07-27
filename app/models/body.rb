11725237ea21e45678f4ba108dea0491278d81da
Zoe Barth <zoe.barth@gmail.com>
2018-07-27 16:12:32 -0500

    validates lat and long

--- app/models/body.rb
+++ app/models/body.rb
@@ -2,7 +2,8 @@ class Body < ApplicationRecord
   has_many :populations
   has_many :fishes, through: :populations
   validates :name, presence: true, length: {maximum: 255}
-  validates :latitude, :longitude, presence:true, numericality: { only_decimal: true }
+  validates :latitude, presence:true, numericality: { only_decimal: true, greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
+  validates :longitude, presence:true, numericality: { only_decimal: true, greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :description, length: {maximum: 1000}, allow_blank: true
 
 end
